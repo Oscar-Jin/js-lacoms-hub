17c7a81549a73ca57a68fdca548106afa4ec948e
Oscar Jin <kintininn@gmail.com>
2020-05-20 12:00:00 +0900

    ver update

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -53,7 +53,7 @@ const Navbar = (props) => {
           <span className="">
             <img src={favicon}></img>
             <span>LACOMS-HUB</span>
-            <sup className="badge badge-light lacoms-version ml-2">1.1.0</sup>
+            <sup className="badge badge-light lacoms-version ml-2">1.1.3</sup>
           </span>
         </a>
 
